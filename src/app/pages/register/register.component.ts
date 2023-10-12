0946bbd72ea79f8b18760b7ae95585c4f6a4c956
221hussein <yuzinpream51@gmail.com>
2023-10-12 21:29:19 +0000

    reactive form angular

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
+
+  form!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+  }
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      first_name: '',
+      last_name: '',
+      email: '',
+      password: '',
+      password_confirm: '',
+
+    });
+  }
+  submit() {
+
+  }
 
 }
